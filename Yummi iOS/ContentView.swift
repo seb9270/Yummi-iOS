fd2093536fc744a2920b0fd896249a7222b1fc8e
Sebastian Micko <Micko.S@etoncollege.org.uk>
2024-01-23 13:06:20 +0000

    Initial Commit

---
+++ Yummi iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yummi iOS
+//
+//  Created by Micko, Sebastian (AMM) on 23/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yummi iOS/Yummi_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yummi_iOSApp.swift
+//  Yummi iOS
+//
+//  Created by Micko, Sebastian (AMM) on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Yummi_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
