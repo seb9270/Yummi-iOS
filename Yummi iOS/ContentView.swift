f397c1fc0cb9dd0dd2db1eafa4fd1b5640b53c80
Sebastian Micko <Micko.S@etoncollege.org.uk>
2024-02-02 11:21:06 +0000

    nearly done adding ingredient

--- Yummi iOS/ContentView.swift
+++ Yummi iOS/ContentView.swift
@@ -11,8 +11,15 @@ import Foundation
 struct ContentView: View {
     @State private var possibleIngredients = PossibleIngredients()
     @State private var selectedIngredient: Int = 2
-    @State private var newIngredient: String = ""
     @State private var cycle: Int = 0
+    @State private var newName = ""
+    @State private var newQuantityString = ""
+    @State private var newQuantityInt = 0
+    @State private var newUnit = ""
+    @State private var newCategory: Category = .other
+    @State private var newExpiryDate = ""
+    
+    
     
     
     var body: some View {
@@ -30,12 +37,34 @@ struct ContentView: View {
             }
             Section {
                 if cycle == 0 {
-                    TextField("Enter name:", text: $newIngredient)
-                        //cycle += 1
+                    TextField("Enter name:", text: $newName)
                 }
                 else if cycle == 1 {
+                    TextField("Enter quantity:", text: $newQuantityString)
+                }
+                else if cycle == 2 {
+                    TextField("Enter unit:", text: $newUnit)
+                }
+                else if cycle == 3 {
+                    Picker("Choose Category", selection: $newCategory) {
+                        ForEach(Category.allCases, id:\.self) { category in Text(category.rawValue) }
+                    }
                     
                 }
+                else if cycle == 4 {
+                    TextField("Enter expiry date:", text: $newExpiryDate)
+                }
+                Button("SUBMIT", action: {
+                    if cycle < 4 {
+                        cycle += 1
+                    }
+                    else {
+                        cycle = 0
+                        newQuantityInt = Int(newQuantityString) ?? 0
+                        var newIngredient = ingredient(name: newName, quantity: newQuantityInt, unit: newUnit, category: newCategory, expiryDate: newExpiryDate)
+                        //PossibleIngredients.ingredientList.append(newIngredient)
+                    }
+                })
             }
         }
     }
--- Yummi iOS/Models.swift
+++ Yummi iOS/Models.swift
@@ -7,14 +7,14 @@
 
 import Foundation
 
-enum Category {
+enum Category: String, CaseIterable {
     case fruit
     case vegetable
     case meat
     case other
 }
 
-struct ingredients {
+struct ingredient {
     let name: String
     var quantity: Int
     var unit: String
@@ -22,32 +22,17 @@ struct ingredients {
     let expiryDate: String
     
     func displayIngredient() -> String {
-        var cat = ""
-        if category == Category.vegetable {
-            cat = "Vegetable"
-        }
-        else if category == Category.fruit {
-            cat = "fruit"
-        }
-        else if category == Category.meat {
-            cat = "meat"
-        }
-        else if category == Category.other {
-            cat = "other"
-        }
-        
-        
         return """
 Name: \(name)
 Quantity: \(quantity) \(unit)
-Category: \(cat)
+Category: \(category.rawValue)
 Expiry date: \(expiryDate)
 """
     }
 }
 
 struct PossibleIngredients {
-    var ingredientList = [ingredients(name: "Mango", quantity: 3, unit: "mangoes", category: Category.fruit, expiryDate: "27 November 2024"),
-                          ingredients(name: "Onion", quantity: 35, unit: "units", category: Category.vegetable, expiryDate: "30th March 2024"),
-                          ingredients(name: "Steak", quantity: 10, unit: "Kgs", category: Category.meat, expiryDate: "31st July 2025")]
+    var ingredientList = [ingredient(name: "Mango", quantity: 3, unit: "mangoes", category: Category.fruit, expiryDate: "27 November 2024"),
+                          ingredient(name: "Onion", quantity: 35, unit: "units", category: Category.vegetable, expiryDate: "30th March 2024"),
+                          ingredient(name: "Steak", quantity: 10, unit: "Kgs", category: Category.meat, expiryDate: "31st July 2025")]
 }
